127e2fe9ba70c15b5bbe8b5d612190e603996c60
phexcom <phexcom@phexcom.me>
2016-08-07 02:49:58 +0400

    added sharer url

--- lwc/settings/production.py
+++ lwc/settings/production.py
@@ -17,7 +17,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 
-#SHARE_URL = "http://launchwithcode.com/?ref="
+SHARE_URL = "hthttps://still-castle-26645.herokuapp.com/?ref="
 # Static asset configuration
 # import os
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
